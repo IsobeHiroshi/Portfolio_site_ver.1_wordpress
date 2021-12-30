cebd63c0c44357b67bc6ae7814674c48c06f436a
Hiroshi Isobe <isobe69b@gmail.com>
2021-12-30 10:11:37 -0800

    OGP setting added to the head tag in the front-page.php

--- front-page.php
+++ front-page.php
@@ -3,6 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- OGP property setting from here -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://hiroshi-isobe.com/">
+  <meta property="og:title" content="Hiroshi Isobe Portfolio Site">
+  <meta property="og:description" content="A developer with web marketing and communication skills.">
+  <meta property="og:image" content="<?php bloginfo('stylesheet_directory'); ?>/images/OGP-thumbnail.jpg">
+  <!-- OGP property setting ends here -->
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
